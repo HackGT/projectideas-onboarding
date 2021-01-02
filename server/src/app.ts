cb405541d813ef400a0b22ffe5c0c3502d497b6d
mohandodda0 <mohandodda@rocketmail.com>
2021-01-01 23:26:37 -0600

    backend starter code initalized

--- server/src/app.ts
+++ server/src/app.ts
@@ -27,29 +27,3 @@ process.on("unhandledRejection", (err) => {
 import { isAuthenticated } from "./auth/auth";
 import { authRoutes } from "./routes/auth";
 import { ideaRoutes } from "./routes/idea";
-
-app.get("/status", (req, res) => {
-  res.status(200).send("Success");
-});
-
-// app.use("/",);
-app.use("/auth", authRoutes);
-app.use("/ideas", isAuthenticated, ideaRoutes);
-
-app.use(
-  isAuthenticated,
-  express.static(path.join(__dirname, "../../client/build"))
-);
-app.get("*", isAuthenticated, (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
-});
-
-app.use(function (err, req, res, next) {
-  res.status(500).send({ error: true, message: err });
-});
-
-app.listen(port, () => {
-  console.log(
-    `Project Ideas Onboarding Project system v${VERSION_NUMBER} started on port ${process.env.PORT}`
-  );
-});
