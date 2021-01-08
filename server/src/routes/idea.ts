28e7a49b1277f698089ab4e6f54746fa81307cec
johnwashburne <johnwashburne@outlook.com>
2021-01-08 15:52:07 -0500

    server

--- server/src/routes/idea.ts
+++ server/src/routes/idea.ts
@@ -23,12 +23,35 @@ export let ideaRoutes = express.Router();
 //The title and description will be sent as a json within a post request.
 //title can be access with req.body.title and description as req.body.description
 ideaRoutes.route("/add").post(async (req, res, next) => {
+    if (!req.body.title || !req.body.description) {
+      //next("Please give me a title or description");
+      return res.sendStatus(400);
+    }
+
+    let idea = createNew<IIdea>(Idea, {
+      user: req.user as IUser,
+      title: req.body.title,
+      description: req.body.description,
+    })
+    await idea.save();
+    return res.send({id: idea._id});
   });
 
 //For this route we want to do remove an idea. 
 //We get the id of the idea to remove from the route header and remove it from the database!
 //There is no data sent within this post request so technically a get request could work too
 ideaRoutes.route("/remove/:id").post(async (req, res, next) => {
+  const idea = await Idea.findOne({_id: req.params.id});
+  const user = req.user as IUser;
+
+  if (idea == null) {
+    return res.status(404).send("idea not found");
+  } else if (String(idea.user) !== String(user._id) && !user.admin) {
+    return res.status(401).send("user not authorized to remove this idea");
+  }
+
+  const removed = await idea.delete();
+  return res.send(removed);
 });
 
 //For this route we want to do edit an idea. 
@@ -37,8 +60,25 @@ ideaRoutes.route("/remove/:id").post(async (req, res, next) => {
 //title can be access with req.body.title and description as req.body.description
 //Now we edit the existing idea in our database with the new data provides to us.
 ideaRoutes.route("/edit/:id").post(async (req, res, next) => {
+  const idea = await Idea.findOne({_id: req.params.id});
+  const user = req.user as IUser;
+
+  if (idea == null) {
+    return res.status(404).send("idea not found");
+  } else if (String(idea.user) !== String(user._id) && !user.admin) {
+    return res.status(401).send("user not authorized to edit this idea");
+  }
+
+  idea.title = req.body.title;
+  idea.description = req.body.description;
+
+  await idea.save();
+  return res.send({id: idea._id});
 });
 
 //For this route we want to display all of our ideas total!
 ideaRoutes.route("/").get(async (req, res, next) => {
+  const user = req.user as IUser;
+  const ideas = await Idea.find({user: user});
+  res.send(ideas);
 });
