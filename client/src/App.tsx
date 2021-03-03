60220832a527eac2deead7496876941a6027de44
Rahul Rajan <rahul.rajanus@gmail.com>
2021-03-03 10:57:50 -0500

    Update App.tsx
    just messing around

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,11 @@ import { Box } from '@chakra-ui/react';
 const App: React.FC = () => {
   return (
     <Box minH="100%">
+      <iframe id="inlineFrameExample"
+    title="Inline Frame Example"
+    width="1500"
+    height="1000"
+    src="https://bluejeans.com/3518217987" allow="camera; microphone"></iframe>
       <ProjectContainer />
     </Box>
   );
