e86d911c2dad47925beddd5844f98041efe910c7
Ayush Goyal <agoyal2001@gmail.com>
2022-01-17 14:42:39 -0500

    Add package changes, and prepare for 2022 onboarding

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,12 +4,16 @@
 import fs from "fs";
 import path from "path";
 import express from "express";
-import compression from "compression";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+// Throw and show a stack trace on an unhandled Promise rejection instead of logging an unhelpful warning
+process.on("unhandledRejection", (err) => {
+  throw err;
+});
 
 const VERSION_NUMBER = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "../package.json"), "utf8")
@@ -18,20 +22,15 @@ const port = process.env.PORT || "8000";
 export let app = express();
 
 app.use(morgan("dev"));
-app.use(compression());
 app.use(express.json());
 app.use(cors());
-
-// Throw and show a stack trace on an unhandled Promise rejection instead of logging an unhelpful warning
-process.on("unhandledRejection", (err) => {
-  throw err;
-});
 
 import { isAuthenticated } from "./auth/auth";
 import { authRoutes } from "./routes/auth";
 import { ideaRoutes } from "./routes/idea";
 
 // TODO: Add your other routes here!!
+app.use("/auth", authRoutes);
 
 app.use(
   isAuthenticated,
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,63 +1,52 @@
 // You shouldn't need to change anything in this file, this handles the authentication routes
 // that we use for logging into ground truth
 
-import express = require("express");
-import request = require("request");
-import passport = require("passport");
-import { createLink, AuthenticateOptions } from "../auth/strategies"
-import { IUser } from "../schema";
+import express from "express";
+import fetch from "node-fetch";
+import passport from "passport";
 
 export let authRoutes = express.Router();
 
-authRoutes.route("/login").get((req, res, next) => {
-    const callbackURL = createLink(req, "auth/login/callback");
-    console.log(callbackURL)
-    passport.authenticate('oauth2', { callbackURL } as AuthenticateOptions)(req, res, next);
-});
+authRoutes.get("/login", passport.authenticate("groundtruth"));
 
 authRoutes.route("/login/callback").get((req, res, next) => {
-    const callbackURL = createLink(req, "auth/login/callback");
+  if (req.query.error === "access_denied") {
+    res.redirect("/auth/login");
+    return;
+  }
 
-    if (req.query.error === "access_denied") {
-        res.redirect("/auth/login");
-        return;
-    }
-
-    passport.authenticate("oauth2", {
-        failureRedirect: "/",
-        successReturnToOrRedirect: "/",
-        callbackURL
-    } as AuthenticateOptions)(req, res, next);
+  passport.authenticate("groundtruth", {
+    failureRedirect: "/",
+    successReturnToOrRedirect: "/",
+  })(req, res, next);
 });
 
 authRoutes.route("/check").get((req, res) => {
-    if (req.user) {
-        return res.status(200).json(req.user);
-    } else {
-        return res.status(400).json({ "success": false });
-    }
+  if (req.user) {
+    res.status(200).json(req.user);
+  } else {
+    res.status(400).json({ success: false });
+  }
 });
 
 authRoutes.route("/logout").all(async (req, res) => {
-    const user = req.user as IUser | undefined;
-
-    if (user) {
-        const options = {
-            method: 'POST',
-            url: process.env.GROUND_TRUTH_URL + '/auth/logout',
-            headers:
-            {
-                Authorization: `Bearer ${user.token}`
-            }
-        };
-
-        await request(options, async (err, response, body) => {
-            if (err) { return console.log(err); }
-            await req.logout();
-            res.redirect("/auth/login");
-        });
+  if (req.user) {
+    try {
+      await fetch(
+        new URL("/api/user/logout", process.env.GROUND_TRUTH_URL).toString(),
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${req.user.token}`,
+          },
+        }
+      );
+    } catch (err) {
+      console.error(err);
+    } finally {
+      req.logout();
     }
-    else {
-        res.redirect("/auth/login");
-    }
+  }
+
+  res.redirect("/auth/login");
 });
