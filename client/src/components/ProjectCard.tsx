61467d93fcb0f456a95478c063eb519cf8270d09
Ayush Goyal <agoyal2001@gmail.com>
2022-05-13 13:50:25 -0400

    Add new comments to frontend

---
+++ client/src/components/ProjectCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+// TODO: Create ProjectCard component with props
+
+// TODO: Create card with Chakra UI styling and display content
+
+// TODO: Create method to delete idea on button click
--- client/src/components/ProjectContainer.tsx
+++ client/src/components/ProjectContainer.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import { Flex, Heading, Button, SimpleGrid } from "@chakra-ui/react";
+import React from "react";
 
 const ProjectContainer: React.FC = () => {
-  return <div>ProjectContainer</div>;
+  // TODO: Add data fetching
+
+  // TODO: Add modal handling with useDisclosure
+
+  // TODO: handle loading and error states
+
+  return (
+    <>
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        padding="1.5rem"
+        bg="tomato"
+        color="white"
+        height="100%"
+      >
+        <Heading as="h1">Project Ideas</Heading>
+        <Button color="tomato">Add Idea</Button>
+      </Flex>
+
+      <SimpleGrid columns={[2, 3, 5]} spacing={6} padding={10}>
+        {/* Add ProjectCard ideas here */}
+      </SimpleGrid>
+
+      {/* Add modal here */}
+    </>
+  );
 };
 
 export default ProjectContainer;
